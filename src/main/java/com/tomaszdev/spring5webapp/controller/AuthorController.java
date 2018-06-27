1ef39a8bf32d945d0a32ef2832e58300b7f8f104
Tomasz Nurek <rufuswombat@gmail.com>
2018-06-27 17:41:54 +0200

    Created AuthorController and basic view in authors.html using Thymeleaf

--- src/main/java/com/tomaszdev/spring5webapp/controller/AuthorController.java
+++ src/main/java/com/tomaszdev/spring5webapp/controller/AuthorController.java
@@ -1,4 +1,23 @@
 package com.tomaszdev.spring5webapp.controller;
 
+import com.tomaszdev.spring5webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors";
+    }
+
 }
